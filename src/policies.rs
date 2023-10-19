e2e76b3cb468424d9add5528e12e9a48c3556a67
alexcano98 <alexcanocos@gmail.com>
2023-10-19 23:00:14 +0200

    feat: added averageOccupancyFunction

--- src/policies.rs
+++ src/policies.rs
@@ -309,6 +309,7 @@ pub fn new_virtual_channel_policy(arg:VCPolicyBuilderArgument) -> Box<dyn Virtua
 			"LabelSaturate" => Box::new(LabelSaturate::new(arg)),
 			"LabelTransform" => Box::new(LabelTransform::new(arg)),
 			"OccupancyFunction" => Box::new(OccupancyFunction::new(arg)),
+			"AverageOccupancyFunction" => Box::new(AverageOccupancyFunction::new(arg)),
 			"NegateLabel" => Box::new(NegateLabel::new(arg)),
 			"VecLabel" => Box::new(VecLabel::new(arg)),
 			"MapLabel" => Box::new(MapLabel::new(arg)),
@@ -782,6 +783,196 @@ impl LowestSinghWeight
 			use_internal_space,
 			use_neighbour_space,
 			use_estimation,
+		}
+	}
+}
+
+
+///Select the lowest value of the product of the queue length (that is, consumed credits) times the estimated hop count (usually 1 plus the distance from next router to target router)
+#[derive(Debug)]
+pub struct AverageOccupancyFunction
+{
+	///Which multiplies the label.
+	label_coefficient: i32,
+	///Which multiplies the occupancy.
+	occupancy_coefficient: i32,
+	///Which multiplies the product of label and occupancy.
+	product_coefficient: i32,
+	///Just added.
+	constant_coefficient: i32,
+	///Whether to use internal output space in the calculations instead of the counters relative to the next router.
+	///defaults to false
+	use_internal_space: bool,
+	///Whether to add the neighbour space.
+	///Defaults to true.
+	use_neighbour_space: bool,
+	///Whether we consider all the space in each port (when true) or we segregate by virtual channels (when false).
+	virtual_channels: Vec<usize>,
+	///Whether to average the occupation of the virtual channels or add them.
+	average_virtual_channels: bool,
+	///Whether to exclude the current port from the calculations.
+	exclude_minimal_ports: bool,
+	///A vector with link classes to exclude from the calculations.
+	exclude_link_classes: Vec<usize>,
+}
+
+impl VirtualChannelPolicy for AverageOccupancyFunction
+{
+	fn filter(&self, candidates:Vec<CandidateEgress>, router:&dyn Router, info: &RequestInfo, topology:&dyn Topology, _rng: &mut StdRng) -> Vec<CandidateEgress>
+	{
+		//let port_average_neighbour_queue_length=port_average_neighbour_queue_length.as_ref().expect("port_average_neighbour_queue_length have not been computed for policy AverageOccupancyFunction");
+		if router.get_index().expect("we need routers with index") == info.target_router_index
+		{
+			//do nothing
+			candidates
+		}
+		else
+		{
+			let distance = topology.distance(router.get_index().expect("Index should be here"),info.target_router_index);
+
+			candidates.into_iter().map(
+				//|CandidateEgress{port,virtual_channel,label,estimated_remaining_hops}|
+				|candidate|{
+					// let CandidateEgress{port,virtual_channel,label,..} = candidate;
+					let CandidateEgress{label,..} = candidate;
+					//let minimal_neighbour = topology.neighbour(router.get_index().expect("Index should be here"), port);
+
+					let mut q_avg= 0;
+					let mut q_count=0;
+
+					for p_avg in 0..topology.degree(0)  //info.port_available_output_space.expect("port_available_output_space is needed").len() //all the ports
+					{
+						let (neighbour_location,neighbour_link_class) = topology.neighbour(router.get_index().expect("Index should be here"), p_avg);
+						let neighbour_router_index = match neighbour_location
+						{
+							Location::RouterPort {router_index: neighbour_router, router_port: _neighbour_port} =>
+								{
+									neighbour_router
+								},
+							_ =>  panic!(),
+						};
+
+						let neighbour_distance = topology.distance(neighbour_router_index,info.target_router_index);
+
+						if (self.exclude_minimal_ports && neighbour_distance < distance) || self.exclude_link_classes.contains(&neighbour_link_class)
+						{
+							continue;
+						}
+
+						q_count+=1;
+
+						q_avg += if self.use_internal_space
+						{
+							let mut occupied_output_space = 0;
+							for i in 0..self.virtual_channels.len()
+							{
+								let virtual_channel_occupied_output_space=info.virtual_channel_occupied_output_space.expect("virtual_channel_occupied_output_space have not been computed for AverageOccupancyFunction");
+								occupied_output_space += virtual_channel_occupied_output_space[p_avg][self.virtual_channels[i]] as i32;
+							}
+
+							if self.average_virtual_channels
+                            {
+								occupied_output_space = occupied_output_space/ self.virtual_channels.len() as i32;
+                            }
+
+								occupied_output_space
+
+						}
+						else {0} + if self.use_neighbour_space
+						{
+							let mut occupied_output_space = 0;
+							let status=router.get_status_at_emisor(p_avg).expect("This router does not have transmission status");
+							for i in 0..self.virtual_channels.len()
+							{
+								let virtual_channel_occupied_output_space=router.get_maximum_credits_towards(p_avg,self.virtual_channels[i]).expect("we need routers with maximum credits") as i32
+									- status.known_available_space_for_virtual_channel(self.virtual_channels[i]).expect("remote available space is not known.") as i32;
+								occupied_output_space += virtual_channel_occupied_output_space;
+							}
+
+							if self.average_virtual_channels
+							{
+								occupied_output_space = occupied_output_space/ self.virtual_channels.len() as i32;
+
+							}
+
+								occupied_output_space
+
+						}
+						else {0};
+					}
+
+					let q = q_avg /q_count;
+
+					let new_label = self.label_coefficient*label + self.occupancy_coefficient*q + self.product_coefficient*label*q + self.constant_coefficient;
+					CandidateEgress{label:new_label,..candidate}
+				}).collect::<Vec<_>>()
+		}
+	}
+
+	fn need_server_ports(&self)->bool
+	{
+		false
+	}
+
+	fn need_port_average_queue_length(&self)->bool
+	{
+		false
+	}
+
+	fn need_port_last_transmission(&self)->bool
+	{
+		false
+	}
+
+}
+
+impl AverageOccupancyFunction
+{
+	pub fn new(arg:VCPolicyBuilderArgument) -> AverageOccupancyFunction
+	{
+		let mut label_coefficient=None;
+		let mut occupancy_coefficient=None;
+		let mut product_coefficient=None;
+		let mut constant_coefficient=None;
+		let mut use_internal_space=false;
+		let mut use_neighbour_space=false;
+		let mut virtual_channels= None;
+		let mut average_virtual_channels=false;
+		let mut exclude_minimal_ports=true;
+		let mut exclude_link_classes=Vec::new();
+		//let mut only_minimal_link_class=false;
+		match_object_panic!(arg.cv,"AverageOccupancyFunction",value,
+			"label_coefficient" => label_coefficient=Some(value.as_f64().expect("bad value for label_coefficient") as i32),
+			"occupancy_coefficient" => occupancy_coefficient=Some(value.as_f64().expect("bad value for occupancy_coefficient") as i32),
+			"product_coefficient" => product_coefficient=Some(value.as_f64().expect("bad value for product_coefficient") as i32),
+			"constant_coefficient" => constant_coefficient=Some(value.as_f64().expect("bad value for constant_coefficient") as i32),
+			"use_neighbour_space" => use_neighbour_space=value.as_bool().expect("bad value for use_neighbour_space"),
+			"use_internal_space" => use_internal_space=value.as_bool().expect("bad value for use_internal_space"),
+			"exclude_minimal_ports" => exclude_minimal_ports=value.as_bool().expect("bad value for exclude_minimal_ports"),
+			"virtual_channels" => virtual_channels=Some(value.as_array().expect("bad value for virtual_channels").iter()
+				.map(|v| v.as_f64().expect("bad value for virtual_channels") as usize ).collect::<Vec<_>>()),
+			"average_virtual_channels" => average_virtual_channels=value.as_bool().expect("bad value for average_virtual_channels"),
+			"exclude_link_classes" => exclude_link_classes=value.as_array().expect("bad value for exclude_link_classes").iter()
+                .map(|v| v.as_f64().expect("bad value for exclude_link_classes") as usize ).collect::<Vec<_>>(),
+			//"only_minimal_link_class" => only_minimal_link_class=value.as_bool().expect("bad value for only_minimal_link_class"),
+		);
+		let label_coefficient=label_coefficient.expect("There were no multiplier");
+		let occupancy_coefficient=occupancy_coefficient.expect("There were no multiplier");
+		let product_coefficient=product_coefficient.expect("There were no multiplier");
+		let constant_coefficient=constant_coefficient.expect("There were no multiplier");
+		let virtual_channels=virtual_channels.expect("There were no virtual channels");
+
+		AverageOccupancyFunction{
+			label_coefficient,
+			occupancy_coefficient,
+			product_coefficient,
+			constant_coefficient,
+			use_internal_space,
+			use_neighbour_space,
+			virtual_channels,
+			average_virtual_channels,
+			exclude_minimal_ports,
+			exclude_link_classes,
 		}
 	}
 }
